4dbaefc66fa9fe38feee4d147251895666f0ff9c
haeer <haeer98@outlook.com>
2020-01-03 22:23:39 +0800

    第一次实例提交

---
+++ Multi/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi
+{
+    public class Class1
+    {
+        public void Show(int a,int b)
+        {
+            Console.WriteLine((Math.Pow(a, b).ToString()));
+            Console.Read();
+        }
+    }
+}
---
+++ PY/test.py
@@ -0,0 +1,7 @@
+import clr
+clr.AddReference('Multi')
+
+from Multi import *
+
+tt=Class1()
+tt.Show(2,3)
